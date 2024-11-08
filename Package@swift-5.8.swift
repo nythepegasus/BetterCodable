b617076038e0790bc108d667bc9f1c50d44d494d
ny <mac@nythepegas.us>
2024-11-08 08:47:01 -0500

    Fix building on 5.8

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -6,13 +6,11 @@ let package = Package(
     name: "NYDecodable",
     products: [
         .library(name: "NYDecodable", targets: ["NYDecodable"]),
-        .library(name: "NYDecodableErrorLoggers", targets: ["NYDecodable", "NYDecodableErrorLoggers"]),
     ],
     targets: [
         .target(name: "NYDecodable"),
-        .target(name: "NYDecodableErrorLoggers", dependencies: ["NYDecodable"]),
         .testTarget(name: "NYDecodableTests",
-                    dependencies: ["NYDecodable", "NYDecodableErrorLoggers"]
+                    dependencies: ["NYDecodable"]
         ),
     ]
 )
